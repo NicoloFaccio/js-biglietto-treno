86fd1fdc3ecb437e2552132a9933ef2601d72cb7
NicoloFaccio <nicolo.faccio1@gmail.com>
2023-12-22 15:32:50 +0100

    working on the conditional

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+//PART ABOUT THE INITATION OF THE VARIABLES 
+let kilometresDone= prompt("Inserire il N° di chilometri da percorrere:");
+let agePerson= prompt("Inserire l'età del passeggero:");
+const priceBus= 0.21
+
+
+
+//PART ABOUT THE CONSOLE.LOG
+
+console.log("N° di chilometri da percorrere:", kilometresDone);
+console.log("Età del passeggero:", agePerson);
+
+//PART THE CONDITIONAL
+
+if(agePerson < ) 
